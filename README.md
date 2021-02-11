e577b6cb7a139896b2f00e8a58b747ad81f7ddcf
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2021-02-11 12:51:39 +0100

    minor updates and 0-firecfg.config update

--- README.md
+++ README.md
@@ -3,14 +3,11 @@ firecfg.py
 
 [![maintenance-status: experimental](https://img.shields.io/badge/maintenance--status-experimental-orange)](https://gist.github.com/rusty-snake/574a91f1df9f97ec77ca308d6d731e29)
 
-
 An improved firecfg written in python.
 
 ### :warning: This is experimental software, use at your own risk. :warning: ###
 
 maintenance: **experimental**
-
-Version: 0.0.3
 
 Features
 --------
@@ -20,8 +17,9 @@ Features
  * Fix gapplications
  * Fix dbus-services
  * Fix autostart
- * TODO: Fix systemd
+ * TODO: Fix systemd --user
  * TODO: Fix dbus-system-service
+ * TODO: Firejail user access database
 
 License
 -------
--- contrib/gen_groups.py
+++ contrib/gen_groups.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+# Copyright © 2020 rusty-snake
+#
+# This file is part of firecfg.py
+#
+# firecfg.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# firecfg.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 from os import listdir
 
 def listdir_sorted(path):
@@ -46,4 +63,3 @@ for filename in listdir_sorted("/etc/firejail"):
 
 with open("generated_groups.txt", "w") as out_file:
     out_file.write(out)
-
