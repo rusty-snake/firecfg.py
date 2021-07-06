7db728fb0747911d5019157608fd285df09f6647
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2021-07-06 12:00:05 +0200

    Try to simplify maybe-firejail.rs
    Still no idea for the build/install integration.

--- maybe-firejail.rs
+++ maybe-firejail.rs
@@ -1,50 +1,64 @@
+/*
+ * Copyright © 2021 The firecfg.py Authors
+ *
+ * This file is part of firecfg.py
+ *
+ * firecfg.py is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * firecfg.py is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::env;
-use std::ffi::OsStr;
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::path::PathBuf;
+use std::process::exit;
 use std::process::Command;
-
-struct Cmdl<T, U> {
-    program: T,
-    args: U,
-}
+use std::io::Error as IoError;
 
 fn main() -> ! {
-    let (mut _cmdl_a, mut _cmdl_b);
-    let cmdl: Cmdl<&dyn AsRef<OsStr>, &mut dyn Iterator<Item = String>>;
+    let mut args /*: impl Iterator<Item = String> */ = env::args();
+    let arg0: String = args.next().unwrap();
+
+    let mut command: Command;
     if let Ok("firejail") = env::var("container").as_deref() {
-        let arg0 = env::args().next().unwrap();
-        let program_name = Path::new(&arg0).file_name().unwrap();
+        let program_name: &str = basename(&arg0).unwrap();
+        let program: PathBuf = find_program(program_name).unwrap_or_else(|| {
+            eprintln!("maybe-firejail: Could not find '{}'", program_name);
+            exit(1);
+        });
 
-        _cmdl_a = Cmdl {
-            program: find_program(program_name)
-                .unwrap_or_else(|| panic!("Could not find '{}'", program_name.to_string_lossy())),
-            args: env::args().skip(1),
-        };
-        cmdl = Cmdl {
-            program: &_cmdl_a.program,
-            args: &mut _cmdl_a.args,
-        };
+        command = Command::new(program);
     } else {
-        _cmdl_b = Cmdl {
-            program: "firejail",
-            args: env::args(),
-        };
-        cmdl = Cmdl {
-            program: &_cmdl_b.program,
-            args: &mut _cmdl_b.args,
-        };
-    }
+        assert!(!arg0.starts_with('-'));
 
-    let err = Command::new(cmdl.program).args(cmdl.args).exec();
-    panic!("Failed to exec child: {}", err);
+        command = Command::new("firejail");
+        command.arg(arg0);
+    };
+
+    let err: IoError = command.args(args).exec();
+    eprintln!("maybe-firejail: Failed to exec child: {}", err);
+    exit(1);
+}
+
+fn basename(path: &str) -> Option<&str> {
+    path.rsplit_once('/').map(|(_, file_name)| file_name)
 }
 
 fn find_program<P: AsRef<Path>>(program_name: P) -> Option<PathBuf> {
     env::var("PATH")
         .expect("Failed to get $PATH")
         .split(':')
+        .filter(|path| !path.starts_with('/'))
         .filter(|path| !path.contains("firecfg.py"))
         .map(|path| Path::new(path).join(&program_name))
         .find(|path| path.exists())
